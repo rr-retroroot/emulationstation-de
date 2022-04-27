3f1bdf58a9a1a3c7c71195fbf973168463da69f0
Leon Styhre <leon@leonstyhre.com>
2022-04-27 17:32:07 +0200

    (Linux) Yet another ugly hack to detect installed emulators when running as a Flatpak.

--- es-core/src/utils/FileSystemUtil.cpp
+++ es-core/src/utils/FileSystemUtil.cpp
@@ -19,6 +19,7 @@
 #include "utils/FileSystemUtil.h"
 
 #include "Log.h"
+#include "Platform.h"
 #include "utils/StringUtil.h"
 
 #include <fstream>
@@ -209,15 +210,44 @@ namespace Utils
             return "";
 #else
 #if defined(FLATPAK_BUILD)
-            // Ugly hack for the Flatpak build to find emulators installed as Flatpaks and Snaps.
-            // Note that due to limitations of this package format anything installed under /usr
-            // and similar system directories will not be possible to launch as they are shadowed
-            // by the Flatpak sandbox directories. Therefore they are excluded here.
-            std::string pathVariable{
-                "/var/lib/flatpak/exports/bin:/snap/bin:/var/lib/snapd/snap/bin"};
+            // Ugly hack to compensate for the Flatpak sandbox restrictions. We traverse
+            // this hardcoded list of paths and use the "which" command to check outside the
+            // sandbox if the emulator binary exists.
+            std::string pathVariable{"/var/lib/flatpak/exports/bin:/usr/bin:/usr/local/"
+                                     "bin:/usr/local/sbin:/usr/sbin:/sbin:/bin:/usr/games:/usr/"
+                                     "local/games:/snap/bin:/var/lib/snapd/snap/bin"};
+
+            std::vector<std::string> pathList{
+                Utils::String::delimitedStringToVector(pathVariable, ":")};
+
+            // Using a temporary file is the only viable solution I've found to communicate
+            // between the sandbox and the outside world.
+            std::string tempFile{Utils::FileSystem::getHomePath() + "/.emulationstation/" +
+                                 ".flatpak_emulator_binary_path.tmp"};
+
+            std::string emulatorPath;
+
+            for (auto it = pathList.cbegin(); it != pathList.cend(); ++it) {
+                runSystemCommand("flatpak-spawn --host which " + *it + "/" + executable + " > " +
+                                 tempFile);
+                std::ifstream tempFileStream;
+                tempFileStream.open(tempFile);
+                getline(tempFileStream, emulatorPath);
+                tempFileStream.close();
+
+                if (emulatorPath != "") {
+                    emulatorPath = getParent(emulatorPath);
+                    break;
+                }
+            }
+
+            if (exists(tempFile))
+                removeFile(tempFile);
+
+            return emulatorPath;
 #else
             std::string pathVariable{std::string(getenv("PATH"))};
-#endif
+
             std::vector<std::string> pathList{
                 Utils::String::delimitedStringToVector(pathVariable, ":")};
 
@@ -230,6 +260,7 @@ namespace Utils
                     return it->c_str();
             }
             return "";
+#endif
 #endif
         }
 
