fa0f0e3a3ac798c7c11fa567aa379a2c08046a1f
Leon Styhre <leon@leonstyhre.com>
2022-05-19 17:24:56 +0200

    Renamed the --create-systemdirs command line option to --create-system-dirs

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -329,7 +329,7 @@ bool parseArgs(int argc, char* argv[])
         else if (strcmp(argv[i], "--force-input-config") == 0) {
             forceInputConfig = true;
         }
-        else if (strcmp(argv[i], "--create-systemdirs") == 0) {
+        else if (strcmp(argv[i], "--create-system-dirs") == 0) {
             createSystemDirectories = true;
         }
         else if (strcmp(argv[i], "--debug") == 0) {
@@ -359,7 +359,7 @@ bool parseArgs(int argc, char* argv[])
 "  --force-kiosk                   Force the UI mode to Kiosk\n"
 "  --force-kid                     Force the UI mode to Kid\n"
 "  --force-input-config            Force configuration of input device\n"
-"  --create-systemdirs             Create game system directories\n"
+"  --create-system-dirs            Create game system directories\n"
 "  --home [path]                   Directory to use as home path\n"
 "  --debug                         Print debug information\n"
 "  --version, -v                   Display version information\n"
