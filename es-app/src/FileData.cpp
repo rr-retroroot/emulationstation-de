71ec025eca839dfa0d6e3c936c9f69a2e51186fa
Leon Styhre <leon@leonstyhre.com>
2022-07-15 22:01:59 +0200

    Added a %FILENAME% variable to extract the filename including the file extension when used in es_systems.xml

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -835,6 +835,7 @@ void FileData::launchGame(Window* window)
         }
     }
 
+    const std::string fileName{baseName + Utils::FileSystem::getExtension(romPath)};
     const std::string romRaw = Utils::FileSystem::getPreferredPath(getPath());
     const std::string esPath = Utils::FileSystem::getExePath();
     bool runInBackground = false;
@@ -1407,6 +1408,7 @@ void FileData::launchGame(Window* window)
     // Replace the remaining variables with their actual values.
     command = Utils::String::replace(command, "%ROM%", romPath);
     command = Utils::String::replace(command, "%BASENAME%", baseName);
+    command = Utils::String::replace(command, "%FILENAME%", fileName);
     command = Utils::String::replace(command, "%ROMRAW%", romRaw);
     command = Utils::String::replace(command, "%ROMPATH%",
                                      Utils::FileSystem::getEscapedPath(getROMDirectory()));
